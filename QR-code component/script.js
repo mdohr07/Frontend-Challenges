f2075dc826a189a3ab1de4b51098ce0bd90c9255
mdohr07 <mdohr07@gmail.com>
2025-04-29 15:04:37 +0200

    add feedback message

--- QR-code component/script.js
+++ QR-code component/script.js
@@ -2,7 +2,12 @@ const linkURL ="https://www.frontendmentor.io";
 
 function copyToClipboard() {
     navigator.clipboard.writeText(linkURL)
-}
+      .then(() => {
+        // Zeige Feedback-Text
+        document.getElementById("copy-feedback").style.display = "block";
+      })
+  }
+
 
 // Event Listener
 document.getElementById("copy-button").addEventListener("click", copyToClipboard);
