44714fbdfa68bba1800ad3dd8ff9d81343b7a15d
mdohr07 <mdohr07@gmail.com>
2025-05-02 14:52:13 +0200

    add JavaScript with array of fake articles

---
+++ blog-preview-card/script.js
@@ -0,0 +1,18 @@
+const articles = [
+    {
+      title: "HTML & CSS foundations",
+      text: "These languages are the backbone of every website...",
+      author: "Greg Hooper"
+    },
+    {
+      title: "JavaScript Basics",
+      text: "Learn how to make your website interactive...",
+      author: "Linda Ray"
+    },
+    {
+      title: "Accessibility in Web Design",
+      text: "Creating inclusive experiences starts here.",
+      author: "Jamal Lee"
+    }
+  ];
+  
