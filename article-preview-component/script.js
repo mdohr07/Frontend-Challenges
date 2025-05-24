fe27b922cc6680d1cdce9cb55690f270d7477c33
mdohr07 <mdohr07@gmail.com>
2025-05-24 13:24:37 +0200

    add extra media queries and styles

--- article-preview-component/script.js
+++ article-preview-component/script.js
@@ -4,6 +4,13 @@ function toggleShareMenu () {
     menu.classList.toggle("hidden");
     console.log("toggled");
 }
+/*
+document.addEventListener('click', (toggleShareMenu) => {
+    if (!shareMenuContainer.contains(menu.target)) {
+        shareMenuContainer.classList.add('hidden');
+    }
+});
+*/
 
 document.getElementById("share-button").addEventListener("click", toggleShareMenu);
 
